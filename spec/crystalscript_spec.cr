a625db39b247c5f1f53176222fbf500f9dfccdf1
zlotnleo <zlotnleo@gmail.com>
2018-11-08 14:03:44 +0000

    Fix unterminating child process in testing

--- spec/crystalscript_spec.cr
+++ spec/crystalscript_spec.cr
@@ -4,6 +4,15 @@ Tests = [
   {"assignemnts, number and string literals", "simple.cr"}
 ]
 
+describe "wait_or_timeout" do
+  it "doesn't let the code run forever" do
+    expect_raises(Timeout) do
+      proc = Process.new("crystal", args: ["eval", "while true; end"])
+      wait_or_timeout(proc, 0.5.seconds)
+    end
+  end
+end
+
 describe CrystalScript do
   describe "#convert" do
     Tests.each do |description, filename|
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -14,6 +14,21 @@ end
 class Timeout < Exception
 end
 
+def wait_or_timeout(proc, time)
+  d = delay(time) do
+    io = IO::Memory.new
+    Process.run("pgrep", args: ["-P #{proc.pid}"], output: io)
+    child_pid = io.to_s.to_i
+    Process.kill(Signal::TERM, proc.pid, child_pid)
+  end
+  case proc.wait
+  when .normal_exit?
+    d.cancel
+  when .signal_exit?
+    raise Timeout.new("Timout exceeded")
+  end
+end
+
 def run_test(filename)
   with_temp("temp") do
     crystal_in_path = __DIR__ + "/input/" + filename
@@ -23,15 +38,7 @@ def run_test(filename)
     crystal_out = File.new(crystal_out_path, mode: "w")
 
     proc = Process.new("crystal", args: ["run", crystal_in_path], output: crystal_out, error: STDERR)
-    d = delay(2.seconds) do
-      proc.kill(Signal::KILL)
-    end
-    case proc.wait
-    when .normal_exit?
-      d.cancel
-    when .signal_exit?
-      raise Timeout.new("Timout exceeded")
-    end
+    wait_or_timeout(proc, 2.seconds)
 
     js_in_path = __DIR__ + "/temp/" + base_name + ".js"
     File.write(js_in_path, CrystalScript.convert(File.read(crystal_in_path)))
@@ -40,15 +47,7 @@ def run_test(filename)
     js_out = File.new(js_out_path, mode: "w")
 
     proc = Process.new("node", args: [js_in_path], output: js_out, error: STDERR)
-    d = delay(2.seconds) do
-      proc.kill
-    end
-    case proc.wait
-    when .normal_exit?
-      d.cancel
-    when .signal_exit?
-      raise Timeout.new("Timout exceeded")
-    end
+    wait_or_timeout(proc, 2.seconds)
 
     File.read(crystal_out_path).should eq File.read(js_out_path)
   ensure
