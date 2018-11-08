31e157d99f1b111c4721c559cb5acdef8b62479c
zlotnleo <zlotnleo@gmail.com>
2018-11-08 01:33:28 +0000

    Tests compile and run the code in both crystal and JS

--- spec/crystalscript_spec.cr
+++ spec/crystalscript_spec.cr
@@ -1,35 +1,15 @@
 require "./spec_helper"
 
+Tests = [
+  {"assignemnts, number and string literals", "simple.cr"}
+]
+
 describe CrystalScript do
   describe "#convert" do
-    it "handles multiple expressions and number literals" do
-      source = "\
-1_u8
-2_u32; 3.1415_f64; 2.71828_f32
--2
-"
-      target = "\
-new UInt8(\"1\");
-new UInt32(\"2\");
-new Float64(\"3.1415\");
-new Float32(\"2.71828\");
-new Int32(\"-2\");
-"
-      CrystalScript.convert(source).should eq target;
+    Tests.each do |description, filename|
+      it description do
+        run_test(filename)
+      end
     end
-
-    it "handles assignment of number literals to one variable at a time" do
-      source = "\
-a = 3_i32
-b = 4
-a = 12
-"
-      target = "\
-let a = new Int32(\"3\");
-let b = new Int32(\"4\");
-a = new Int32(\"12\");
-"
-      CrystalScript.convert(source).should eq target;
-    end
-  end
-end
+   end
+ end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,59 @@
 require "spec"
 require "../src/crystalscript"
+
+def with_temp(name, &block)
+  begin
+    Dir.mkdir_p(__DIR__ + "/" + name)
+    yield
+    Dir.rmdir(__DIR__ + "/" + name)
+  rescue e : Errno
+    STDERR.puts e.to_s
+  end
+end
+
+class Timeout < Exception
+end
+
+def run_test(filename)
+  with_temp("temp") do
+    crystal_in_path = __DIR__ + "/input/" + filename
+    base_name = File.basename(crystal_in_path, ".cr")
+
+    crystal_out_path = __DIR__ + "/temp/" + base_name + ".cr_out"
+    crystal_out = File.new(crystal_out_path, mode: "w")
+
+    proc = Process.new("crystal", args: ["run", crystal_in_path], output: crystal_out, error: STDERR)
+    d = delay(2.seconds) do
+      proc.kill(Signal::KILL)
+    end
+    case proc.wait
+    when .normal_exit?
+      d.cancel
+    when .signal_exit?
+      raise Timeout.new("Timout exceeded")
+    end
+
+    js_in_path = __DIR__ + "/temp/" + base_name + ".js"
+    File.write(js_in_path, CrystalScript.convert(File.read(crystal_in_path)))
+
+    js_out_path = __DIR__ + "/temp/" + base_name + ".js_out"
+    js_out = File.new(js_out_path, mode: "w")
+
+    proc = Process.new("node", args: [js_in_path], output: js_out, error: STDERR)
+    d = delay(2.seconds) do
+      proc.kill
+    end
+    case proc.wait
+    when .normal_exit?
+      d.cancel
+    when .signal_exit?
+      raise Timeout.new("Timout exceeded")
+    end
+
+    File.read(crystal_out_path).should eq File.read(js_out_path)
+  ensure
+    File.delete(js_in_path.as(String))
+    crystal_out.delete unless crystal_out.nil?
+    js_out.delete unless js_out.nil?
+  end
+end
