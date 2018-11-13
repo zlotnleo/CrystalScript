982378a6c4c2f0312685b6bb86c4e1f917e23240
zlotnleo <zlotnleo@gmail.com>
2018-11-13 10:08:01 +0000

    Put tests on a separate branch

--- spec/crystalscript_spec.cr
+++ spec/crystalscript_spec.cr
@@ -1,1 +1,24 @@
 require "./spec_helper"
+
+Tests = [
+  {"assignemnts, number and string literals", "simple.cr"}
+]
+
+describe "wait_or_timeout" do
+  it "doesn't let the code run forever" do
+    expect_raises(Timeout) do
+      proc = Process.new("crystal", args: ["eval", "while true; end"], error: STDERR)
+      wait_or_timeout(proc, 0.5.seconds)
+    end
+  end
+end
+
+# describe CrystalScript do
+#   describe "#convert" do
+#     Tests.each do |description, filename|
+#       it description do
+#         run_test(filename)
+#       end
+#     end
+#    end
+#  end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,66 @@
 require "spec"
 require "../src/crystalscript"
+
+# def with_temp(name, &block)
+#   begin
+#     Dir.mkdir_p(__DIR__ + "/" + name)
+#     yield
+#     Dir.rmdir(__DIR__ + "/" + name)
+#   rescue e : Errno
+#     STDERR.puts e.to_s
+#   end
+# end
+
+class Timeout < Exception
+end
+
+# def wait_or_timeout(proc, time)
+#   d = delay(time) do
+#     puts "Timeout " + proc.pid.to_s
+#     io = IO::Memory.new
+#     pgrep = Process.new("pgrep", args: ["-P #{proc.pid}"], error: STDERR)
+#     status = pgrep.wait
+#     puts "pgrep exited with: " + status.exit_code.to_s
+#     output = pgrep.output
+#     puts output.to_s
+#     # child_pid = io.to_s.to_i
+
+#     # puts child_pid
+#     # puts "childpid: " + child_pid.to_s
+#     # Process.kill(Signal::TERM, child_pid, proc.pid)
+#   end
+#   case proc.wait
+#   when .normal_exit?
+#     d.cancel
+#   when .signal_exit?
+#     raise Timeout.new("Timout exceeded")
+#   end
+# end
+
+# def run_test(filename)
+#   with_temp("temp") do
+#     crystal_in_path = __DIR__ + "/input/" + filename
+#     base_name = File.basename(crystal_in_path, ".cr")
+
+#     crystal_out_path = __DIR__ + "/temp/" + base_name + ".cr_out"
+#     crystal_out = File.new(crystal_out_path, mode: "w")
+
+#     proc = Process.new("crystal", args: ["run", crystal_in_path], output: crystal_out, error: STDERR)
+#     wait_or_timeout(proc, 2.seconds)
+
+#     js_in_path = __DIR__ + "/temp/" + base_name + ".js"
+#     File.write(js_in_path, CrystalScript.convert(File.read(crystal_in_path)))
+
+#     js_out_path = __DIR__ + "/temp/" + base_name + ".js_out"
+#     js_out = File.new(js_out_path, mode: "w")
+
+#     proc = Process.new("node", args: [js_in_path], output: js_out, error: STDERR)
+#     wait_or_timeout(proc, 2.seconds)
+
+#     File.read(crystal_out_path).should eq File.read(js_out_path)
+#   ensure
+#     File.delete(js_in_path.as(String))
+#     crystal_out.delete unless crystal_out.nil?
+#     js_out.delete unless js_out.nil?
+#   end
+# end
