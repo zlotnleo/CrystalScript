e9d4778567243b3fe1733e68e3676c1f5f4ffe5c
zlotnleo <zlotnleo@gmail.com>
2018-12-13 03:55:28 +0300

    Try to fix travis again

--- create_VERSION.sh
+++ create_VERSION.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-export CRYSTAL_INSTALL_DIR="$(crystal env CRYSTAL_PATH |  awk -F: '{print $1}')/../"
+CRYSTAL_INSTALL_DIR="$(crystal env CRYSTAL_PATH |  awk -F: '{print $1}')/../"
 crystal env CRYSTAL_VERSION > "$CRYSTAL_INSTALL_DIR/VERSION"
+chown $USER "$CRYSTAL_INSTALL_DIR/VERSION"
