0a2a9aeb77c851892ca4e0470d2117728648d228
zlotnleo <zlotnleo@gmail.com>
2018-12-13 03:48:02 +0300

    Travis maybe chown would work?

--- create_VERSION.sh
+++ create_VERSION.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-crystal env CRYSTAL_VERSION > "$(crystal env CRYSTAL_PATH |  awk -F: '{print $1}')/../VERSION"
+export CRYSTAL_INSTALL_DIR="$(crystal env CRYSTAL_PATH |  awk -F: '{print $1}')/../"
+crystal env CRYSTAL_VERSION > "$CRYSTAL_INSTALL_DIR/VERSION"
