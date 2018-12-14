66c18962a38240eae48d19468bf4119ad5e6eaf8
zlotnleo <zlotnleo@gmail.com>
2018-12-14 19:37:59 +0300

    use sudo only for writing VERSION

--- create_VERSION.sh
+++ create_VERSION.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CRYSTAL_INSTALL_DIR="$(crystal env CRYSTAL_PATH |  awk -F: '{print $1}')/../"
-crystal env CRYSTAL_VERSION > "$CRYSTAL_INSTALL_DIR/VERSION"
+sudo crystal env CRYSTAL_VERSION > "$CRYSTAL_INSTALL_DIR/VERSION"
 chown $USER "$CRYSTAL_INSTALL_DIR/VERSION"
