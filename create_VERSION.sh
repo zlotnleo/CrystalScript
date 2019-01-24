5992a7ab16a0d57123ee31b1e950b9331b4892ba
zlotnleo <zlotnleo@gmail.com>
2019-01-24 16:12:30 +0000

    Fix create_VERSION script

--- create_VERSION.sh
+++ create_VERSION.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-CRYSTAL_INSTALL_DIR="$(crystal env CRYSTAL_PATH |  awk -F: '{print $1}')/../"
-crystal env CRYSTAL_VERSION > "$CRYSTAL_INSTALL_DIR/VERSION"
-chown $USER "$CRYSTAL_INSTALL_DIR/VERSION"
+VERSION_FILE="$(crystal env CRYSTAL_PATH |  awk -F: '{print $1}')/../VERSION"
+crystal env CRYSTAL_VERSION > $VERSION_FILE
