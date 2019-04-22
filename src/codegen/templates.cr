d224394ae083bb4355cb4059db2a572a9736bcaa
zlotnleo <zlotnleo@gmail.com>
2019-04-22 19:13:45 +0100

    More appropriate name for instance variables

--- src/codegen/templates.cr
+++ src/codegen/templates.cr
@@ -9,7 +9,7 @@ module CrystalScript::CodeGen::Templates
   TYPE_DECLARATION = Crustache.parse <<-TYPE_DECLARATION
   {{{TypeName}}} = function (){
     {{#is_object}}
-      this.$class_vars = Object.create(null);
+      this.$instance_vars = Object.create(null);
     {{/is_object}}
     {{#has_superclass}}
       {{{SuperClass}}}.call(this);
@@ -26,7 +26,6 @@ module CrystalScript::CodeGen::Templates
   {{{TypeName}}}.prototype = Object.assign(Object.create({{#has_superclass}}{{{SuperClass}}}.prototype{{/has_superclass}}{{^has_superclass}}null{{/has_superclass}}){{#included_modules}}, {{{Module}}}.prototype{{/included_modules}});
   {{{TypeName}}}.prototype.$included_modules = {{#has_superclass}}{{{SuperClass}}}.prototype.$included_modules{{/has_superclass}}{{^has_superclass}}[]{{/has_superclass}}{{#has_included_modules}}.concat([{{#included_modules}}{{{Module}}},{{/included_modules}}]){{/has_included_modules}};
   {{{TypeName}}}.prototype.constructor = {{{TypeName}}};
-
   INIT_TYPE
 
   CREATE_METHOD_CLASS = Crustache.parse <<-CREATE_METHOD_CLASS
