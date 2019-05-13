6e96c459d488e5992676853d2ee6df5cef6c8b2c
zlotnleo <zlotnleo@gmail.com>
2019-05-13 01:06:24 +0100

    Remove commented-out code

--- src/utils/to_str_path.cr
+++ src/utils/to_str_path.cr
@@ -1,31 +1,4 @@
 class CrystalScript
-  # def self.get_generic_argument_name(union_type : Nil)
-  #   nil
-  # end
-
-  # def self.get_generic_argument_name(union_type : UnionType)
-  #   union_type.union_types.map { |sub_t| (self.to_str_path sub_t).join("::") }.join("|")
-  # end
-
-  # # Has to be handled separately since tuple's #type_vars is a single TupleInstanceType
-  # def self.get_generic_argument_name(tuple_type : TupleInstanceType)
-  #   result = tuple_type.tuple_types.map { |sub_t| (self.to_str_path sub_t).join("::") }.join(",")
-  #   CrystalScript.logger.info("#{tuple_type}: #{result}")
-  #   return result
-  # end
-
-  # def self.get_generic_argument_name(named_tuple_type : NamedTupleInstanceType)
-  #   named_tuple_type.entries.map { |entry| entry.name + ": " + (self.to_str_path entry.type).join("::") }.join(",")
-  # end
-
-  # def self.get_generic_argument_name(virtual_type : VirtualType)
-  #   (self.get_generic_argument_name virtual_type.base_type) + "+"
-  # end
-
-  # def self.get_generic_argument_name(type : Type)
-  #   self.to_str_path(type).join("::")
-  # end
-
   def self.to_str_path(union_type : UnionType)
     ["(" + union_type.union_types.map { |sub_t| (self.to_str_path sub_t).join("::") }.join("|") + ")"]
   end
@@ -41,7 +14,6 @@ class CrystalScript
   end
 
   def self.to_str_path(generic_instance_type : GenericInstanceType)
-    CrystalScript.logger.info generic_instance_type
     names = self.to_str_path(generic_instance_type.generic_type)
 
     names[-1] += "(" + generic_instance_type.type_vars.values.map do |node|
